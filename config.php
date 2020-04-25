3d82c28894fa10eb994139903932c1da2a271b68
Stanislav Pekarovič <spekarovic@gmail.com>
2020-04-25 14:30:07 +0200

    octave for experiments added

--- config.php
+++ config.php
@@ -1,7 +1,13 @@
 <?php
 
+// OCTAVE
 define("OCTAVE_EXEC","octave");
 define("OCTAVE_EXEC_WITH_OPTIONS", "octave -W --eval ");
 define("CMD_REDIRECT_ERR"," 2>&1");
+
+define("BALL_ON_STICK_LABELS",array("x","y","angle"));
+define("INVERSE_PENDULUM_LABELS",array("x","y","angle"));
+define("CAR_SHOCK_ABSORBER",array("x","y","bodyworkHeight"));
+define("AIRCRAFT_TILT_LABELS",array("x","y","readFlapAngle"));
 
 ?>
