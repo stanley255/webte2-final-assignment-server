4753450b82071d8c8caa4c9825106c2d8f47988c
Stanislav Pekarovič <34084914+stanley255@users.noreply.github.com>
2020-04-25 14:37:05 +0200

    typo fix

--- config.php
+++ config.php
@@ -8,6 +8,6 @@ define("CMD_REDIRECT_ERR"," 2>&1");
 define("BALL_ON_STICK_LABELS",array("x","y","angle"));
 define("INVERSE_PENDULUM_LABELS",array("x","y","angle"));
 define("CAR_SHOCK_ABSORBER",array("x","y","bodyworkHeight"));
-define("AIRCRAFT_TILT_LABELS",array("x","y","readFlapAngle"));
+define("AIRCRAFT_TILT_LABELS",array("x","y","rearFlapAngle"));
 
 ?>
